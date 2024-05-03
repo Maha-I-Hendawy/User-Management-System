1542a45d393b5a76efebb784dce705a2d66f895f
Maha-I-Hendawy <maha.i.hendawy@gmail.com>
2024-05-03 18:23:30 +0300

    templates updated

--- pro/__init__.py
+++ pro/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy  
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '59f672b6fbe60ee8016f46e335f4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydbdb.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
